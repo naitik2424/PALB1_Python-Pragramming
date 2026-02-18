f93a8b954a953fe068b86640137e4e547e195d3a
Naitik <naitik242424@gmail.com>
2026-02-18 09:50:53 +0530

    Added DSA Python programs

---
+++ merge_without_extra_space.py
@@ -0,0 +1,27 @@
+
+
+import math
+
+class Solution:
+    def merge(self, a, b, n, m):
+        gap = math.ceil((n + m) / 2)
+
+        while gap > 0:
+            i = 0
+            j = gap
+
+            while j < n + m:
+                if i < n and j < n:
+                    if a[i] > a[j]:
+                        a[i], a[j] = a[j], a[i]
+                elif i < n and j >= n:
+                    if a[i] > b[j - n]:
+                        a[i], b[j - n] = b[j - n], a[i]
+                else:
+                    if b[i - n] > b[j - n]:
+                        b[i - n], b[j - n] = b[j - n], b[i - n]
+
+                i += 1
+                j += 1
+
+            gap = 0 if gap == 1 else math.ceil(gap / 2)
