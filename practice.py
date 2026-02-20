0c5c52a24b35b477785362860671d29b15e5ce5d
Naitik <naitik242424@gmail.com>
2026-02-20 12:13:57 +0530

    message

---
+++ practice.py
@@ -0,0 +1,11 @@
+fruit_dict = {'apple': 600,'banana': 400,'orange': 300,'kiwi': 700}
+
+# print(fruit_dict)
+
+for keys in fruit_dict:
+  print(keys)
+if ('apple'and 'banana') in fruit_dict:
+    print("true")
+else:
+    print("false")
+
